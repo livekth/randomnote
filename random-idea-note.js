ff0adbec838d179653a8089ec49c9e42093fc339
Tae Kim <d.thkim@gmail.com>
2022-02-06 20:49:47 +0900

    Add files via upload

---
+++ random-idea-note.js
@@ -0,0 +1,87 @@
+; (() => {
+    // init
+    const button_id = "randomIdeaNote"
+
+    const button = document.getElementById(button_id)
+    if (button) {
+        console.log(button_id + ' already loaded')
+        return
+    }
+
+    let dailyNoteIndex = 0,
+        randomDailyNotesList = shuffleArray(userDailyNotesList())
+
+    addButton()
+
+
+    // functions
+    function userDailyNotesList() {
+        return window.roamAlphaAPI.q(`[:find (pull ?page [:block/uid :block/children]) :where [?page :node/title]]`)
+            //.filter(x => x[0].uid.match(/\d\d-\d\d-\d\d\d\d/)) // removes pages that aren't daily notes (uid in format mm-dd-YYYY)
+            .filter(x => x[0].uid.match(/^🌱)/)) // modified to find pages with emoji
+            .filter(x => x[0].children) // removes pages containing no children
+            .filter(x => {
+                if (x[0].children.length == 1) {
+                    const child_id = x[0].children[0].id
+                    const child_block = window.roamAlphaAPI.pull("[:block/children, :block/string]", child_id)
+
+                    if (!child_block[':block/children'] && child_block[':block/string'] == '') {
+                        return false
+                    }
+                }
+                return true
+            }) // removes pages with one child that is blank
+            .map(x => x[0].uid) // returns only the dates
+    }
+
+    function shuffleArray(array) {
+        const a = array.slice()
+
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]]
+        }
+
+        return a
+    }
+
+    function addButton() {
+        const topbar = document.querySelector('.rm-topbar')
+
+        if (topbar) {
+            const button = Object.assign(document.createElement('div'), {
+                id: button_id,
+                className: "bp3-button bp3-minimal bp3-small bp3-icon-random",
+                title: `Random Idea Note (0/${randomDailyNotesList.length})`,
+                style: "margin-left: 4px;",
+                onclick() { openRandomDailyNote() }
+            })
+
+            topbar.appendChild(button)
+            console.log(button_id + ' loaded')
+        } else {
+            // wait for Roam to finish loading
+            setTimeout(addButton, 500)
+        }
+    }
+
+    function openRandomDailyNote() {
+        navigateToPage(randomDailyNotesList[dailyNoteIndex++])
+
+        document.getElementById(button_id).title = `Random Idea Note (${dailyNoteIndex}/${randomDailyNotesList.length})` // update queue position in tooltip
+
+        if (dailyNoteIndex == randomDailyNotesList.length) {
+            dailyNoteIndex = 0
+            randomDailyNotesList = shuffleArray(userDailyNotesList())
+        }
+    }
+
+    function navigateToPage(page_title) {
+        window.location.href = `https://roamresearch.com/#/app/${graphName()}/page/${page_title}`
+    }
+
+    function graphName() {
+        const href_arr = window.location.href.split('/')
+        return href_arr[href_arr.indexOf('app') + 1]
+    }
+})();
